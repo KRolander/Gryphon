e49a376f9afada95418704ca941f29db6a383aef
Alexandru Lolea <A.N.Lolea@student.tudelft.nl>
2025-05-10 10:45:15 +0200

    🎉

---
+++ frontend/src/components/EmailForm.vue
@@ -0,0 +1,23 @@
+<script lang="js">
+import RegisterService from '@/services/RegisterService';
+
+export default {
+    methods: {
+        submitEmail() {
+            // Get data from input field
+            let inputEmail = this.$refs.emailField.value
+            RegisterService.register({email: inputEmail})
+            console.log(`The submitted email is: ${inputEmail}`);
+        }
+    }
+}
+</script>
+
+<template>
+    <div>
+        <input type="text" ref="emailField">
+        <button @click="submitEmail">Submit</button>
+    </div>
+</template>
+
+<style lang="css" scoped></style>
