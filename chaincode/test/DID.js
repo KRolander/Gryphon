282e81dbdd95441c07ff74890f92e772755ff393
Alessandro Neri <a.neri@student.tudelft.nl>
2025-05-15 22:39:47 +0200

    ✅ Added test for DIDExists

--- chaincode/test/DID.js
+++ chaincode/test/DID.js
@@ -47,7 +47,7 @@ describe("test DID chaincode", () => {
         sandbox.restore();
     });
 
-    describe("Issue a DID", () => {
+    describe("Store a DID", () => {
 
         it("should register the DID and DID document to the ledger", async () => {
             let contract = new DIDContract();
@@ -87,6 +87,28 @@ describe("test DID chaincode", () => {
             await contract.storeDID(ctx, didkey, diddoc1);
             return contract.storeDID(ctx, didkey, diddoc2)
               .should.eventually.be.rejectedWith("The DID document with DID did:hlf:123 already exists");
-        })
-    })
+        });
+    });
+
+    describe("Check if a DID exists", () => {
+
+        it("should return true if the DID was stored in the ledger", async () => {
+            let contract = new DIDContract();
+            should.exist(contract);
+
+            let ctx = new Context();
+            ctx.stub = mockStub;
+
+            let didkey = "did:hlf:123";
+            let diddoc = {
+                id: didkey,
+                valid: true,
+            };
+
+            await contract.storeDID(ctx, didkey, diddoc);
+
+            let ret = await contract.DIDExists(ctx, didkey);
+            expect(ret).to.be.true;
+        });
+    });
 });
