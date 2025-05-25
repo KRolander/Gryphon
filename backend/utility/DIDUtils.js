34428f2a8d44fd5904d54ac448d5a8dc8b75a977
Panzariu Matei-Alexandru <M.A.Panzariu@student.tudelft.nl>
2025-05-26 01:12:02 +0200

    :sparkles: Added functinality so that a DID Document supports multiple controllers

--- backend/utility/DIDUtils.js
+++ backend/utility/DIDUtils.js
@@ -1,35 +1,5 @@
 const bs58= require('bs58');
 const crypto= require ('node:crypto');
-async function createDIDDocument(DID, controller, publicKey) {
-    const doc = {
-        "@context": "https://www.w3.org/ns/did/v1",
-        "id": DID,
-        "controller": controller,
-        "verificationMethod": [
-            {
-            "id": DID.concat("#keys-1"),
-            "type": "to be ",
-            "controller": controller,
-            "publicKeyBase58": publicKey
-            }
-        ],
-        "authentication": [
-            DID.concat("#keys-1")
-        ],
-        "assertionMethod": [
-            DID.concat("#keys-1")
-        ],
-        // "service": [
-        //     {
-        //     "id": "did:example:123456789abcdefghi#vcs",
-        //     "type": "VerifiableCredentialService",
-        //     "serviceEndpoint": "https://example.com/vc/"
-        //     }
-        // ]
-    }
-
-    return doc;
-}
 
 async function createDID(){
     const randomString = bs58.default.encode(Buffer.from(crypto.randomBytes(16)));
@@ -37,6 +7,5 @@ async function createDID(){
 }
 
 module.exports = {
-    createDIDDocument,
     createDID
 }
