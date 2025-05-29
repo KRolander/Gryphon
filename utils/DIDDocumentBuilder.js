cc0f8b9037427d74f66d666c882020bbcbc0e2c7
Panzariu Matei-Alexandru <M.A.Panzariu@student.tudelft.nl>
2025-05-29 14:39:51 +0200

    :art: Updated the VC file

--- utils/DIDDocumentBuilder.js
+++ utils/DIDDocumentBuilder.js
@@ -3,33 +3,21 @@ exports.__esModule = true;
 var DIDDocumentBuilder = /** @class */ (function () {
     function DIDDocumentBuilder(DID, controllers, publicKey) {
         this.DID = DID;
-<<<<<<< HEAD
-        this.controller = controller;
-        this.publicKeyPem = publicKey;
-=======
         this.controllers = Array.isArray(controllers) ? controllers : [controllers];
         this.publicKey = publicKey;
->>>>>>> main
     }
     DIDDocumentBuilder.prototype.build = function () {
         var keyId = "".concat(this.DID, "#keys-1");
         return {
             "@context": "https://www.w3.org/ns/did/v1",
             id: this.DID,
-            controllers: this.controllers,
+            controllers: this.controllers.length === 1 ? this.controllers[0] : this.controllers,
             verificationMethod: [
                 {
-<<<<<<< HEAD
-                    id: "".concat(this.controller, "#keys-1"),
-                    type: "EcdsaSecp256r1VerificationKey2019",
-                    controller: this.controller,
-                    publicKeyPem: this.publicKeyPem
-=======
                     id: keyId,
                     type: "to be",
                     controllers: this.controllers[0],
                     publicKeyBase58: this.publicKey
->>>>>>> main
                 }
             ],
             authentication: [keyId],
