d99474958853e4f5c75c131e136dbb0d587bfcda
Lolea Alexandru <A.N.Lolea@student.tudelft.nl>
2025-05-30 13:18:09 +0200

    ✨ added routing for authentication pages

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -6,17 +6,31 @@ import { createWebHistory, createRouter } from "vue-router";
 import HomePage from "../views/HomePage.vue";
 import DIDsPage from "../views/DIDsPage.vue";
 import VCsPage from "../views/VCsPage.vue";
+
+// Auth
 import AuthPage from "../views/AuthPage.vue";
+import SignupPage from "../components/auth/SignupForm.vue";
+import LoginPage from "../components/auth/LoginForm.vue";
+import RecoverPasswordPage from "../components/auth/ForgotPasswordForm.vue";
 
 /* ----------------------- CONFIG ----------------------- */
 const routes = [
-  { path: "/", component: HomePage },
-  { path: "/dids", component: DIDsPage },
-  { path: "/vcs", component: VCsPage },
+  { name: "home", path: "/", component: HomePage },
+  { name: "DIDs", path: "/dids", component: DIDsPage },
+  { name: "VCs", path: "/vcs", component: VCsPage },
   {
     path: "/auth",
     component: AuthPage,
-    children: [{ path: "signup" }, { path: "login" }],
+    children: [
+      { name: "signup", path: "signup", component: SignupPage },
+      { name: "login", path: "login", component: LoginPage },
+      {
+        name: "recover-password",
+        path: "recover-password",
+        component: RecoverPasswordPage,
+      },
+      { name: "catch-all", path: "/:pathMatch(.*)*", redirect: "/auth/login" }, // Redirect to login if no match
+    ],
   },
 ];
 const history = createWebHistory();
