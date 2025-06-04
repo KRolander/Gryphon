3df5888f4a2ed2944da295a3db4d929a5575291f
Alessandro Neri <a.neri@student.tudelft.nl>
2025-06-04 12:52:43 +0200

    🗃️ Added persistence of user id in IndexedDB

--- frontend/src/store/userStore.ts
+++ frontend/src/store/userStore.ts
@@ -1,7 +1,7 @@
 /* ======================= IMPORTS ======================= */
 // store core
 import { defineStore } from 'pinia';
-
+import { get, set } from 'idb-keyval'
 // types
 import type { User } from '../types/User';
 
@@ -16,8 +16,15 @@ export const useUserStore = defineStore('user', {
     getUser: (state): User => state.user,
   },
   actions: {
-    setUser(user: User) {
+    async setUser(user: User) {
       this.user = user;
+      await set('user', user);
+    },
+    async loadUser() {
+      const savedUser = await get<User>('user')
+      if (savedUser) {
+        this.user = savedUser
+      }
     },
     clearUser() {
       this.user = null;
