41196033d3ab77da9d5251c21c0293475a2c3b8b
Alessandro Neri <a.neri@student.tudelft.nl>
2025-06-08 14:06:45 +0200

    🚨 Fix linter warning in backend/utility

--- backend/utility/DIDUtils.js
+++ backend/utility/DIDUtils.js
@@ -1,11 +1,11 @@
-const bs58= require('bs58');
-const crypto= require ('node:crypto');
+const bs58 = require("bs58");
+const crypto = require("node:crypto");
 
-async function createDID(){
-    const randomString = bs58.default.encode(Buffer.from(crypto.randomBytes(16)));
-    return 'did:hlf:'+randomString;
+async function createDID() {
+  const randomString = bs58.default.encode(Buffer.from(crypto.randomBytes(16)));
+  return "did:hlf:" + randomString;
 }
 
-module.exports = { 
-    createDID
-}
+module.exports = {
+  createDID,
+};
