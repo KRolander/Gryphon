78b0f79fc0dc1398188341658c9e37d1fb3b6798
Mihai Frânculescu <M.F.Franculescu@student.tudelft.nl>
2025-06-13 10:29:44 +0200

    ✨ Basic VC Page

--- frontend/src/views/VCsPage.vue
+++ frontend/src/views/VCsPage.vue
@@ -1,15 +1,37 @@
 <template>
-  <v-container class="fill-height d-flex justify-center" max-width="600" max-height="600">
+  <v-container class="fill-height d-flex flex-column align-center justify-center" max-width="800">
     <div>
       <div class="mb-8 text-center">
         <div class="text-body-1 font-weight-light mb-n1">Welcome to</div>
         <h1 class="text-h1 font-weight-bold">Your VCs</h1>
-        <div class="text-body-1 font-weight-light mb-n1">(will be added soon)</div>
       </div>
     </div>
+
+    <WalletManager v-slot="{ wallet, ready }">
+      <template v-if="ready">
+        
+      </template>
+    </WalletManager>
   </v-container>
 </template>
 
-<script lang="js"></script>
+<script lang="js">
+/* ----------------------- IMPORTS ----------------------- */
+import WalletManager from '@/components/wallet/WalletManager.vue';
+
+/* ----------------------- CONFIG ----------------------- */
+export default {
+  name: "VCsPage",
+  components: { WalletManager },
+  data() {
+    return {
+      VCs: []
+    };
+  },
+  methods: {
+
+  }
+};
+</script>
 
 <style lang="css" scoped></style>
