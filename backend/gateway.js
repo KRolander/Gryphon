93ea147f0b892f70493e8ae96630bcb67dca3e14
KRolander <kromesrolandgyula@gmail.com>
2026-01-07 13:22:33 +0100

    Change Peer endpoint from host.docker.internal:7051 to peer0.org1.example.com:7051 to avoid possible issues with localhost finding in Linux Ubuntu 20.04.

--- backend/gateway.js
+++ backend/gateway.js
@@ -29,7 +29,7 @@ const {
 const mspId = envOrDefault("MSP_ID", "Org1MSP");
 
 // Gateway peer endpoint
-const peerEndpoint = envOrDefault("PEER_ENDPOINT", "host.docker.internal:7051");
+const peerEndpoint = envOrDefault("PEER_ENDPOINT", "peer0.org1.example.com:7051");
 
 // Gateway peer SSL host name
 const peerHostAlias = envOrDefault("PEER_HOST_ALIAS", "peer0.org1.example.com"); //connecting to localhost would cause the TLS handshake to fail.
